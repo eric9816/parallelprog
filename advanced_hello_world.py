2cfc5161a0a1c632b82dac4314f1297ac1e9d51d
eric9816 <eric981623@gmail.com>
2023-11-12 23:09:32 +0300

    commit 3

---
+++ advanced_hello_world.py
@@ -0,0 +1,26 @@
+import string
+import time
+
+letters = string.ascii_lowercase
+eng_letters = list(letters + '_')
+hw_lst = list('hello_world')
+#hw_lst = list('zzzzzzzzzzzzzzzz')
+
+result = ['' for _ in range(len(hw_lst))]
+for idx, hw_letter in enumerate(hw_lst, 0):
+    time.sleep(0.03)
+    for eng_letter in eng_letters:
+        result[idx] = eng_letter
+        if eng_letter == hw_letter:
+            time.sleep(0.03)
+            break
+        time.sleep(0.03)
+        print(''.join(result))
+print(''.join(result))
+
+
+        # print(result)
+        # if eng_letter == hw_letter:
+        #     time.sleep(0.5)
+        #     result += eng_letter
+        #     break
